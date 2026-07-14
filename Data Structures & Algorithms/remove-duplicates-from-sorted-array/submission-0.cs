5d8ce572dcc3227d848cc8b8e19f6ec23cc81e99
street-07 <ujjwalmehta6969@gmail.com>
2026-07-15 02:21:49 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int l = 1;
+        for (int r = 1; r < nums.Length; r++) {
+            if (nums[r] != nums[r - 1]) {
+                nums[l] = nums[r];
+                l++;
+            }
+        }
+
+        return l;
+    }
+}
